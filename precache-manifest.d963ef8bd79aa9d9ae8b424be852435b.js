adc12b41cfdc42df61aa574780de5de2fa87156d
Agnieszka Jarosik <agajarosik@o2.pl>
2019-10-15 18:48:34 +0200

    Updates

---
+++ precache-manifest.d963ef8bd79aa9d9ae8b424be852435b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d34d5c0a3dc17261e913c593b06655a",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "b97a920262c679bca93e",
+    "url": "/drum-machine/static/css/main.a3ae7408.chunk.css"
+  },
+  {
+    "revision": "b2edddce2383baafa7e0",
+    "url": "/drum-machine/static/js/2.98ee75cb.chunk.js"
+  },
+  {
+    "revision": "b97a920262c679bca93e",
+    "url": "/drum-machine/static/js/main.1dc8671b.chunk.js"
+  },
+  {
+    "revision": "59341921b8a28aa24500",
+    "url": "/drum-machine/static/js/runtime-main.5e3e1888.js"
+  },
+  {
+    "revision": "c3a140795dbf32d455e289276dcee7ee",
+    "url": "/drum-machine/static/media/black_lozenge.c3a14079.png"
+  },
+  {
+    "revision": "a5257aefdb8294736ef34f73a37a5cb0",
+    "url": "/drum-machine/static/media/boom.a5257aef.wav"
+  },
+  {
+    "revision": "a306730ab4b02d32a6cd1d5c74d2e95b",
+    "url": "/drum-machine/static/media/clap.a306730a.wav"
+  },
+  {
+    "revision": "a7f7fe836460d7620f26e749c9dbf7a6",
+    "url": "/drum-machine/static/media/electronic-ride.a7f7fe83.wav"
+  },
+  {
+    "revision": "863da6e29025afa495e5db5f103b3427",
+    "url": "/drum-machine/static/media/harp.863da6e2.wav"
+  },
+  {
+    "revision": "8a342bd70c45346fa609a2710a0e0f02",
+    "url": "/drum-machine/static/media/hi-hats.8a342bd7.wav"
+  },
+  {
+    "revision": "987fa9f0c9b328ee9a9fe2801274b5fd",
+    "url": "/drum-machine/static/media/kick.987fa9f0.wav"
+  },
+  {
+    "revision": "fa7ac356f9ecca173bedb3e168862d5d",
+    "url": "/drum-machine/static/media/ride-bell.fa7ac356.wav"
+  },
+  {
+    "revision": "bc394356c54aa84cbb4bb0c8ce08447e",
+    "url": "/drum-machine/static/media/snare.bc394356.wav"
+  },
+  {
+    "revision": "5fa031fa4c835750b3be35ea9849ee24",
+    "url": "/drum-machine/static/media/tom.5fa031fa.wav"
+  }
+]);
